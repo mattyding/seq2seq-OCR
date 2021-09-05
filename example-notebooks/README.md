cbfd0d46ae876d1c18c7d98926266fd6cca6a708
mattyding <66889540+mattyding@users.noreply.github.com>
2021-09-05 18:03:13 -0500

    Added example usage

---
+++ example-notebooks/README.md
@@ -0,0 +1,4 @@
+This directory contains several example notebooks that show how to use the seq2seq model on noisy text.
+
+
+I would recommend first looking at [Basic Usage](basic_usage.ipynb) to get an idea of how to use the model. 
--- seq2seqocr.py
+++ seq2seqocr.py
@@ -1,53 +1,45 @@
 import os
 import re
 import json
-import pickle
+import pickle  # for lexicons
 import string
 import numpy as np
 from tensorflow import keras
 from threading import Thread, Lock
 
-from general_util import clean_text
+from general_util import clean_text, clean_text_no_spaces
 from settings import *
 
-CLASSIFICATION_TOKEN = "[CLS]"
+CLASSIFICATION_TKN = "[CLS]"
 UNIDENTIFIABLE_TOKEN = "[...]"
 
-def main():
-    model = Seq2SeqOCR('s2s')
-    print(model.predict("loods"))
+# error messages
+MODEL_SCOPE_MSG = "The model is only trained to predict single words. Use the 'process_text' to run the model on multi-word strings.'"
 
+
+def main():
+    model = Seq2SeqOCR(SAVED_MODEL)  # TODO: MAYBE SWITCH MODEL BACK (CURRENTLY OVERTRAINED)
     #print(model.predict('clty'))
     #print(model.predict('affiftant'))
-    return
 
     with open("SAMPLE_FILE.txt", 'r') as f:
         sample_text = f.read()
-        cleaned_text = model.preprocess(sample_text)
-        print(cleaned_text)
-
-        #ew_text = []
-        #for word in cleaned_text.split():
-            #if word.startswith(CLASSIFICATION_TOKEN):
-                #predicted_word = model.predict(word[len(CLASSIFICATION_TOKEN):])
-                #print(word[len(CLASSIFICATION_TOKEN):], predicted_word)
-                #new_text.append(predicted_word)
-            #else:
-                #new_text.append(word)
-        #print("".join(new_text))
+        asdf = model.process_text(sample_text)
+        print(asdf)
 
 
 class Seq2SeqOCR:
-    def __init__(self, model_path : str):
+    def __init__(self, model_path : str =SAVED_MODEL):
         super(Seq2SeqOCR, self).__init__()
 
-        self.input_texts, target_texts, input_chars, target_chars = parse_training_data()
-        self.Input = CharacterTable(self.input_texts, input_chars)
+        input_texts, target_texts, input_chars, target_chars = parse_training_data()
+        self.Input = CharacterTable(input_texts, input_chars)
         self.Target = CharacterTable(target_texts, target_chars)
 
         self.english_lexicon, self.common_lexicon = retrieve_lexicons()
 
         self.memoized_words = {}  # for preprocessing memoization
+        populate_compound_memory(self.memoized_words)
 
         model = keras.models.load_model(model_path)
 
@@ -70,20 +62,36 @@ class Seq2SeqOCR:
         self.decoder_model = keras.Model(
             [decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states
         )
+        
+    def predict_word(self, input_word : str) -> str:
+        """
+        Encodes/decodes a single word and returns the model's output.
+        """
+        if (type(input_word) != str) or (input_word.strip().find(' ') != -1):
+            raise ValueError(MODEL_SCOPE_MSG)
 
-    def predict(self, input_text : str) -> str:
-        encoded_word = self.encode_sequence(input_text)
-        decoded_word = self.decode_sequence(encoded_word)
+        clean_word = clean_text_no_spaces(input_word)
+        encoded_word = self.encode_sequence(clean_word)
+        decoded_word = ""
+        decoded_word += self.decode_sequence(encoded_word)
+        decoded_word = decoded_word.strip('\n')
         return decoded_word
 
     def encode_sequence(self, input_text : str) -> np.array:
+        """
+        Encodes a string as a one-hot array of indices and characters.
+        """
         word_encoding = np.zeros((1, self.Input.max_seq_length, self.Input.num_tokens), dtype="float32")
         for t, char in enumerate(input_text):
             word_encoding[0, t, self.Input.token_index[char]] = 1.0
         word_encoding[0, t + 1 :, self.Input.token_index[" "]] = 1.0
+        word_encoding = word_encoding[0:1]
         return word_encoding
     
     def decode_sequence(self, input_seq : np.array) -> str:
+        """
+        Decodes an encoded string by passing it into the Decoder LSTM
+        """
         # Encode the input as state vectors.
         states_value = self.encoder_model.predict(input_seq)
 
@@ -117,13 +125,38 @@ class Seq2SeqOCR:
             states_value = [h, c]
         return decoded_sentence
 
+    def process_text(self, input_text : str, safe_mode : bool = True):
+        """
+        Preprocesses and evaluates the model on some text. Returns the infererence result.\
+
+        If safe_mode is True, then the model's predicted output does not replace the original
+        if the output is not a recognized English word.
+        """
+        if (type(input_text) != str):
+            raise ValueError("Inputted text must be a string.")
+        
+        pped_text = self.preprocess(input_text).split()
+
+        for i in range(len(pped_text)):
+            if pped_text[i].startswith(CLASSIFICATION_TKN):
+                word = pped_text[i][len(CLASSIFICATION_TKN):]
+                predicted_word = self.predict_word(word)
+                if (not safe_mode) or (predicted_word in self.english_lexicon):
+                    pped_text[i] = predicted_word
+                else:
+                    pped_text[i] = word  # removes CLASSIFICATION_TKN
+
+        return " ".join(pped_text)
     
     def preprocess(self, input_text : str) -> str:
         """
         Preprocesses the input text by removing punctuation and correcting spacing errors.
         Saves the processed text to an intermediate file and also returns processed texts.
         """
-
+        # if it is clear that this text has already been pre-processed, returns as is
+        if CLASSIFICATION_TKN in input_text:
+            return input_text
+        
         # Clean the input text
         input_text = clean_text(input_text.lower())
         # Split the input text into words
@@ -147,7 +180,7 @@ class Seq2SeqOCR:
                 preprocessed_text.append(word + split_text[i+1])
                 couching = True
             # 3. Backward Couching: if the previous entry and the current word make a word, couches them
-            elif (i != 0) and ((split_text[i-1] + word) in self.english_lexicon):
+            elif (i != 0) and ((split_text[i-1].lstrip(CLASSIFICATION_TKN) + word) in self.english_lexicon):
                 preprocessed_text.append(split_text[i-1] + word)
             # 4. Missing Spaces: if the current word can be split into valid words(with avg length > 3)
             elif len(check_compound(word, self.english_lexicon, self.memoized_words)) != len(word):
@@ -155,11 +188,10 @@ class Seq2SeqOCR:
                 preprocessed_text.append(check_compound(word, self.common_lexicon, self.memoized_words))
             # 5. Too Long (likely compound): If larger than max_seq_length, returns original word
             elif (len(word) > MAX_SEQ_LENGTH):
-                preprocessed_text.append(UNIDENTIFIABLE_TOKEN)
+                preprocessed_text.append(word)
             # 6. Unknown and Inferable: if other pre-checks don't pass, feeds it to the seq2seq model
             else:
-                preprocessed_text.append(CLASSIFICATION_TOKEN + word)
-                #preprocessed_text.append(CLASSIFICATION_TOKEN)
+                preprocessed_text.append(CLASSIFICATION_TKN + word)
         
         return " ".join(preprocessed_text)
 
@@ -173,28 +205,28 @@ Model Preparation Scripts
 class CharacterTable:
     def __init__(self, text_set, char_set):
         super(CharacterTable, self).__init__()
-        self.characters = sorted(list(char_set))
         self.num_tokens = len(char_set)
-        self.max_seq_length = MAX_SEQ_LENGTH #max([len(txt) for txt in text_set])
+        self.max_seq_length = max([len(txt) for txt in text_set])
         self.token_index = dict([(char, i) for i, char in enumerate(char_set)])
         self.reverse_token_index = dict((i, char) for char, i in self.token_index.items())
 
 def parse_training_data():
     input_characters = set()
     target_characters = set()
-    for c in string.ascii_lowercase:
+    for c in string.ascii_lowercase + ' ':
         input_characters.add(c)
         target_characters.add(c)
     target_characters.add(BREAK_CHAR)
     target_characters.add(ENDSEQ_CHAR)
-    input_characters.add(' ')
-    target_characters.add(' ')
+
+    input_characters = sorted(list(input_characters))
+    target_characters = sorted(list(target_characters))
 
     input_texts = []
     target_texts = []
     with open(DATA_PATH, 'r', encoding="utf-8") as f:
-        lines = f.read().split("\n")[:-1]  # last line has no BREAK_CHAR
-        for line in lines:
+        lines = f.read().split("\n")  # last line has no BREAK_CHAR
+        for line in lines[:min(NUM_SAMPLES, len(lines) - 1)]:
             # tab is our break char and newline is end seq char
             input_text, target_text = line.split(BREAK_CHAR)
             target_text = BREAK_CHAR + target_text + ENDSEQ_CHAR
@@ -203,7 +235,6 @@ def parse_training_data():
     
     return input_texts, target_texts, input_characters, target_characters
 
-
 def retrieve_lexicons():
     with open(ENGLISH_LEXICON_PKL, "rb") as f1, open(COMMON_ENG_LEXICON_PKL, "rb") as f2:
         english_lexicon = pickle.load(f1)
@@ -211,6 +242,7 @@ def retrieve_lexicons():
         f1.close()
         f2.close()
     return english_lexicon, common_lexicon
+
 
 """
 Preprocessing Scripts
@@ -263,19 +295,16 @@ def check_compound_recursive(word_list, english_set, found_splits):
             # splits the last elem in the word list at this index and recurses
             check_compound_recursive(word_list[:-1] + [word_list[-1][:i], word_list[-1][i:]], english_set, found_splits)
 
-    def populate_compound_memory(memoized_words : dict) -> None:
-        """
-        Populates the memoization set with several common short-word combinations.
-        """
-        memoized_words['inthe'] = 'in the'
-        memoized_words['ofthe'] = 'of the'
-        memoized_words['forthe'] = 'for the'
-        memoized_words['canbe'] = 'can be'
-        memoized_words['goto'] = 'go to'
-
-
-
-
+def populate_compound_memory(memoized_words : dict) -> None:
+    """
+    Populates the memoization set with several common short-word combinations.
+    """
+    memoized_words['inthe'] = 'in the'
+    memoized_words['ofthe'] = 'of the'
+    memoized_words['forthe'] = 'for the'
+    memoized_words['canbe'] = 'can be'
+    memoized_words['goto'] = 'go to'
+    memoized_words['orthem'] = 'or them'
 
 
 if __name__ == '__main__':
